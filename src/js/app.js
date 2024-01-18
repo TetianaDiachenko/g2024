7dbddfdcdd3703f4990a553bbe4ff8c539b804d6
TetianaDiachenko <tetianamyhun@gmail.com>
2024-01-18 13:55:21 +0200

    clean folders for template

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,3 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-// import Swiper from 'swiper';
-// import { Navigation, Pagination } from 'swiper/modules'; // this lines from documentation of swiper
-
-import Swiper, { Navigation, Pagination } from 'swiper'; // from Frilanser
-
-// const swiper = new Swiper(...);
